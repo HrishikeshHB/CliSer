88ba423cec1e1cc3739148c659dd5241d12a01aa
HrishikeshHB <evd16i006@iiitdm.ac.in>
2019-02-06 10:11:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hrishikesh/cliser/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.hrishikesh.cliser;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+
+    private ProgressDialog pDialog;
+    private ListView lv;
+
+    // URL to get contacts JSON
+    private static String url = "http://test-agrimax.000webhostapp.com/v1/index.php/readAll?kitid=1";
+
+    ArrayList<HashMap<String, String>> contactList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactList = new ArrayList<>();
+
+        lv = (ListView) findViewById(R.id.list);
+
+        new GetContacts().execute();
+    }
+
+    private class GetContacts extends AsyncTask<Void,Void,Void>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HTTPHandler sh = new HTTPHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray readings = jsonObj.getJSONArray("readings");
+
+                    // looping through All Contacts
+                    for (int i = 0; i < readings.length(); i++) {
+                        JSONObject c = readings.getJSONObject(i);
+
+                        String ph = c.getString("ph");
+                        String ec = c.getString("ec");
+                        String tds = c.getString("tds");
+                        HashMap<String, String> contact = new HashMap<>();
+                        ph = "pH = "+ph;
+                        ec = "ec = "+ec;
+                        tds = "tds = "+tds;
+                        // adding each child node to HashMap key => value
+                        contact.put("pH", ph);
+                        contact.put("tds", ec);
+                        contact.put("ec", tds);
+                        //contact.put("mobile", mobile);
+                        //Toast.makeText(MainActivity.this, "ph = "+ph, Toast.LENGTH_SHORT).show();
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, contactList,
+                    R.layout.list_item, new String[]{"pH", "tds",
+                    "ec"}, new int[]{R.id.pH,
+                    R.id.tds, R.id.ec});
+
+            lv.setAdapter(adapter);
+        }
+
+    }
+}
